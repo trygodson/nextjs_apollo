043282503516d95255b017aa5faa71c1f66689ae
trygodson <gnqnpn@gmail.com>
2022-05-04 14:40:26 +0100

    initial next scafold & prisma database migration and testing

---
+++ components/Layout/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Header = () => {
+  return (
+    <header className="container">
+      <div className="">
+        <Link href="/">
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
